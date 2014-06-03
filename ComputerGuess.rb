b9d126c2ad2b80a68cbcab0bfde7fdf228d3eabc
Sebastian Armano <sebarmano@gmail.com>
2014-06-03 16:25:40 -0400

    Added game where computer guess user number

---
+++ ComputerGuess.rb
@@ -0,0 +1,56 @@
+class ComputerGuess
+
+  def initialize
+    @number_of_guesses = 5
+    @high_ans = 100
+    @low_ans = 1
+    #move this to a method check that the number is between 1 and 100
+    puts "Think a number between 1 and 100, I'm going to try to guess it"
+    guess
+  end
+
+  def check_answer_with_user #ask about using a local or instance variable
+    puts "I think your number is #{@guessed_number}!"
+    puts "If thats correct, enter Y."
+    puts "If your number is higher, press H"
+    puts "If your number is lower, press L"
+    gets.chomp.upcase!
+  end
+
+  def check_answer
+    check = check_answer_with_user
+    if check == 'Y'
+      puts "YES! I knew it!"
+      return 0
+    elsif check == 'H'
+      puts "Mmm... so your number is higher... let me think..."
+      return 1
+    elsif check == 'L'
+      puts "Lower than that!? Ok... let's see..."
+      return -1
+    else
+      puts "I don't understand what you say... try again..."
+    end
+    check_answer
+  end
+
+  def guess
+    while @number_of_guesses != 0
+      @guessed_number = ((@high_ans - @low_ans)/2) + @low_ans
+      check = check_answer
+      if check == 1 # answer is higher
+        @low_ans = @guessed_number
+      elsif check == -1 # answer is lower
+        @high_ans = @guessed_number
+      else
+        return
+      end
+      @number_of_guesses -= 1
+      if @number_of_guesses == 0
+        "Ok, you win... I don't know your number."
+      end
+    end
+  end
+end
+
+ComputerGuess.new
